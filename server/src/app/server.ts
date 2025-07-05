184a5c59135111a7627c32ac28366e1006d998ea
AlicinhaDavid <alicedaviddossantos@gmail.com>
2025-07-04 22:37:51 -0300

    chore: add cors on server

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import { createPrismaUserRepository } from "../infraestructure/db/PrismaUserRepository";
 import { makeUserRoutes } from "../infraestructure/web/UserController";
 
 export const createServer = () => {
   const app = express();
+
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+
   app.use(express.json());
 
   const prisma = new PrismaClient();

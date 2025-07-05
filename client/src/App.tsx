9ed07a6c97c67e6a29bc40d85e20bd9dbe226bbe
AlicinhaDavid <alicedaviddossantos@gmail.com>
2025-07-04 22:39:57 -0300

    feat: applying loading states and error handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import { useState } from "react";
 
 type UserFormData = {
   name: string;
@@ -8,45 +9,52 @@ type UserFormData = {
 
 function App() {
   const { register, handleSubmit, reset } = useForm<UserFormData>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const onSubmit = async (data: UserFormData) => {
-    try {
-      const response = await axios.post("http://localhost:3000/users", data, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+    setLoading(true);
+    setError(null);
+    setSuccessMessage(null);
 
-      console.log("Response:", response.data);
-      alert("User created successfully!");
+    try {
+      await axios.post("http://localhost:3000/users", data);
+      setSuccessMessage("User created successfully!");
       reset();
-    } catch (err) {
-      alert("Something went wrong!");
-      console.error(err);
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        setError(err?.response?.data?.message || "API error.");
+      } else {
+        setError("Unexpected error");
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div style={{ padding: "2rem" }}>
+    <div style={{ padding: "2rem", maxWidth: "400px" }}>
       <h1>Create User</h1>
+
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Name: </label>
-          <input {...register("name")} placeholder="Your name" required />
+        <div style={{ marginBottom: "1rem" }}>
+          <label>Name:</label>
+          <input {...register("name")} required />
         </div>
-        <div>
-          <label>Email: </label>
-          <input
-            {...register("email")}
-            type="email"
-            placeholder="Your email"
-            required
-          />
+        <div style={{ marginBottom: "1rem" }}>
+          <label>Email:</label>
+          <input {...register("email")} type="email" required />
         </div>
-        <button type="submit" style={{ marginTop: "1rem" }}>
-          Submit
+        <button type="submit" disabled={loading}>
+          {loading ? "Submitting..." : "Submit"}
         </button>
       </form>
+
+      {error && <p style={{ color: "red", marginTop: "1rem" }}>{error}</p>}
+      {successMessage && (
+        <p style={{ color: "green", marginTop: "1rem" }}>{successMessage}</p>
+      )}
     </div>
   );
 }

cef0101d9a1e8bbd7e299e8b0319bd3f09dc4415
AlicinhaDavid <alicedaviddossantos@gmail.com>
2025-07-05 21:20:41 -0300

    feat: add page navigation with react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route, Link } from "react-router-dom";
 import CreateUser from "./pages/CreateUser";
 import DeleteUser from "./pages/DeleteUser";
 import EditUser from "./pages/EditUser";
@@ -7,12 +8,36 @@ import ListUsers from "./pages/ListUsers";
 function App() {
   return (
     <>
-      <h1>User Management</h1>
-      <CreateUser />
-      <ListUsers />
-      <EditUser />
-      <DeleteUser />
-      <FindUser />
+      <div style={{ padding: "2rem" }}>
+        <h1>User Management</h1>
+        <nav style={{ marginBottom: "2rem" }}>
+          <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="/create">Create</Link>
+            </li>
+            <li>
+              <Link to="/edit">Edit</Link>
+            </li>
+            <li>
+              <Link to="/delete">Delete</Link>
+            </li>
+            <li>
+              <Link to="/find">Find</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ListUsers />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/edit" element={<EditUser />} />
+          <Route path="/delete" element={<DeleteUser />} />
+          <Route path="/find" element={<FindUser />} />
+        </Routes>
+      </div>
     </>
   );
 }
